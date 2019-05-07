1e0df9fe289cfb7aa3e4d8691c3591dd3bcce18f
Vojislav Dukic <voja91@gmail.com>
2019-05-07 16:52:38 +0200

    Initialization added

--- lib/util.py
+++ lib/util.py
@@ -1,6 +1,29 @@
 from __future__ import print_function
 import subprocess
-
+import os
 
 def cmd(command):
     return subprocess.check_output(command, shell=True).decode('utf-8')
+
+
+def initialize_output_folder(state, home_dir):
+    home_dir = os.path.expanduser(home_dir)
+    if not os.path.exists(home_dir):
+        os.mkdir(home_dir)
+    if not os.path.isdir(home_dir):
+        raise Exception('File passed as home directory')
+
+    files = ['trace', 'machine_config']
+    for f in files:
+        f_path = home_dir+f+'.log'
+        if not os.path.exists(f_path):
+            file = open(f_path, 'w')
+            #write header
+            if f == 'trace':
+                columns = 'time,cpu,memory,ingress,egress\n'
+                file.write(columns)
+            file.close()
+        state[f] = open(f_path, 'a')
+
+def store_machine_configuration(state):
+    pass
--- meter.py
+++ meter.py
@@ -5,6 +5,8 @@ from time import sleep
 from argparse import ArgumentParser
 
 from lib.util import cmd
+from lib.util import initialize_output_folder
+from lib.util import store_machine_configuration
 from lib.cpu_monitor import measure_cpu, initialize_cpu_monitor
 from lib.memory_monitor import measure_memory, initialize_memory_monitor
 from lib.network_monitor import measure_network, initialize_network_monitor
@@ -12,20 +14,25 @@ from lib.network_monitor import measure_network, initialize_network_monitor
 state = {}
 
 parser = ArgumentParser()
-parser.add_argument("-f", "--frequency", type=float, default=1.0,
+parser.add_argument('-f', '--frequency', type=float, default=1.0,
     help='Frequency of measurements')
+parser.add_argument('-home', '--home-dir', type=str, default='~/.wastemeter/',
+    help='wastemeter home directory')
 
 
-def init_state():
+def init_state(home_dir):
+    initialize_output_folder(state, home_dir)
+
     initialize_cpu_monitor(state)
     initialize_memory_monitor(state)
     initialize_network_monitor(state)
 
+    store_machine_configuration(state)
 
 def main():
     args = parser.parse_args()
 
-    init_state()
+    init_state(args.home_dir)
 
     while True:
         cpu = measure_cpu(state)
