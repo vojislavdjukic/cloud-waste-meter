3673cc8be2f71d95937f062c8ef5948f9d62b6a5
Vojislav Dukic <voja91@gmail.com>
2019-05-14 14:08:09 +0200

    Reporting script updated

--- lib/analysis.py
+++ lib/analysis.py
@@ -10,9 +10,36 @@ def find_configuration(timestamp, configurations):
     if timestamp >= configurations.at[number_of_configurations-1, 'time']:
         return configurations.iloc[[number_of_configurations-1]]
 
+def find_configurations(start_time, end_time, configurations):
+    events = []
+    events.append((start_time,-1))
+    events.append((end_time,-1))
+
+    times = configurations['time'].tolist()
+    for i, t in enumerate(times):
+        events.append((t, i))
+
+    events.append((0, 0))
+
+    events = sorted(events, key = lambda x: x[0])
+
+    i = events.index((start_time,-1))
+
+    map(lambda x: if x[1] , events[:-1])
+
+    [ (events[i]) for i in range(len(events)-1)]
+
+    while events[i] != (end_time,-1):
+
+    return events
+
 def create_report(step, trace_path, machine_config_path):
     trace_df = pd.read_csv(trace_path)
     machine_config_df = pd.read_csv(machine_config_path)
 
-    res = find_configuration(trace_df['time'][0], machine_config_df)
+    time_start = trace_df.at[0, 'time']
+    number_of_logs = len(trace_df.index)
+    time_end = trace_df.at[number_of_logs-1, 'time']
+
+    res = find_configurations(time_start, time_end, machine_config_df)
     print(res)
